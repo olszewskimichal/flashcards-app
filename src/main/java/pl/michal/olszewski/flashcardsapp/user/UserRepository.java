b307f38cef4e72169e25a13437a1837096906c71
Michał <olszewskimichal@outlook.com>
2018-01-04 19:07:20 +0100

    Podstawowe repozytoria

---
+++ src/main/java/pl/michal/olszewski/flashcardsapp/user/UserRepository.java
@@ -0,0 +1,9 @@
+package pl.michal.olszewski.flashcardsapp.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
