da5992a16ccb0b01406a25cc75ae5903edf0e18a
Michał <olszewskimichal@outlook.com>
2018-01-04 19:07:43 +0100

    Wzorzec builder do wszystkich encji

--- src/main/java/pl/michal/olszewski/flashcardsapp/cards/Card.java
+++ src/main/java/pl/michal/olszewski/flashcardsapp/cards/Card.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -14,11 +15,13 @@ import pl.michal.olszewski.flashcardsapp.topic.Topic;
 @Data
 @EqualsAndHashCode(callSuper = true,exclude = "topics")
 @ToString(exclude = "topics")
+@Builder
 public class Card extends BaseEntity {
 
   private String question;
   private String answer;
 
   @ManyToMany(mappedBy = "cards")
+  @Builder.Default
   private List<Topic> topics = new ArrayList<>();
 }
