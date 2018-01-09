5752f28af1d522a23fd3cac45728f96d2d59b869
Michał <olszewskimichal@outlook.com>
2018-01-09 20:55:01 +0100

    Poprawki do testów

--- src/test/java/pl/michal/olszewski/flashcardsapp/equals/EqualsTest.java
+++ src/test/java/pl/michal/olszewski/flashcardsapp/equals/EqualsTest.java
@@ -2,6 +2,7 @@ package pl.michal.olszewski.flashcardsapp.equals;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
+import pl.michal.olszewski.flashcardsapp.attempt.Attempt;
 import pl.michal.olszewski.flashcardsapp.cards.Card;
 import pl.michal.olszewski.flashcardsapp.topic.Topic;
 import pl.michal.olszewski.flashcardsapp.user.User;
@@ -24,7 +25,25 @@ class EqualsTest {
 
   @Test
   void testEqualsContractTest() {
-    EqualsVerifier.forClass(pl.michal.olszewski.flashcardsapp.test.Test.class).withRedefinedSuperclass().verify();
+    Attempt attemptOne = Attempt.builder().id(1L).build();
+    Attempt attemptTwo = Attempt.builder().id(2L).build();
+    Topic topicOne = Topic.builder().name("name1").id(1L).build();
+    Topic topicTwo = Topic.builder().name("name2").id(2L).build();
+
+    EqualsVerifier.forClass(pl.michal.olszewski.flashcardsapp.test.Test.class)
+        .withPrefabValues(Attempt.class, attemptOne, attemptTwo)
+        .withPrefabValues(Topic.class, topicOne, topicTwo)
+        .withRedefinedSuperclass().verify();
+  }
+
+  @Test
+  void attemptEqualsContractTest() {
+    pl.michal.olszewski.flashcardsapp.test.Test testOne = pl.michal.olszewski.flashcardsapp.test.Test.builder().id(1L).build();
+    pl.michal.olszewski.flashcardsapp.test.Test testTwo = pl.michal.olszewski.flashcardsapp.test.Test.builder().id(2L).build();
+
+    EqualsVerifier.forClass(Attempt.class)
+        .withPrefabValues(pl.michal.olszewski.flashcardsapp.test.Test.class, testOne, testTwo)
+        .withRedefinedSuperclass().verify();
   }
 
   @Test
